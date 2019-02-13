5c3bf45f9ea6612ea678e413ca6abcc40ddde0fa
Namjun Kim <namjunemy@gmail.com>
2019-02-13 16:14:36 +0900

    @RepositoryRestController로 REST API 구현(스프링 부트 데이터 레스트 설정 오버라이딩)

---
+++ data-rest/src/main/java/io/namjune/community/rest/controller/BoardRestController.java
@@ -0,0 +1,38 @@
+package io.namjune.community.rest.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import io.namjune.community.rest.domain.Board;
+import io.namjune.community.rest.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.PagedResources.PageMetadata;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequiredArgsConstructor
+@RepositoryRestController
+public class BoardRestController {
+
+    private final BoardRepository boardRepository;
+
+    @GetMapping("/boards")
+    public @ResponseBody
+    Resources<Board> simpleBoard(@PageableDefault Pageable pageable) {
+        Page<Board> boardList = boardRepository.findAll(pageable);
+
+        PageMetadata pageMetadata = new PageMetadata(pageable.getPageSize(), boardList.getNumber(),
+            boardList.getTotalElements());
+        PagedResources<Board> resources = new PagedResources<>(boardList.getContent(),
+            pageMetadata);
+        resources
+            .add(linkTo(methodOn(BoardRestController.class).simpleBoard(pageable)).withSelfRel());
+        return resources;
+    }
+}
