8b138fa7d169b24bc15d328dfa2d96bb6fe308c2
김남준 <njkim@zuminternet.com>
2019-02-13 09:55:25 +0900

    intellij .http로 api 테스트 하기

---
+++ rest-web/src/main/java/io.namjune.community.rest/CommunityWebApplication.java
@@ -0,0 +1,12 @@
+package io.namjune.community.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityWebApplication.class, args);
+    }
+}
