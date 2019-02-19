c4af344164ef8418880cb89976f9c5c0c2d35f69
Namjun Kim <namjunemy@gmail.com>
2019-02-19 20:31:20 +0900

    @RestResource Uri경로 관리(기본Uri : /search)

--- data-rest/src/main/java/io/namjune/community/rest/repository/BoardRepository.java
+++ data-rest/src/main/java/io/namjune/community/rest/repository/BoardRepository.java
@@ -2,8 +2,11 @@ package io.namjune.community.rest.repository;
 
 import io.namjune.community.rest.domain.Board;
 import io.namjune.community.rest.domain.projection.BoardOnlyContainTitle;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource(excerptProjection = BoardOnlyContainTitle.class)
@@ -12,4 +15,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     @Override
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     <S extends Board> S save(S entity);
+
+    @RestResource
+    List<Board> findByTitle(@Param("title") String title);
 }
